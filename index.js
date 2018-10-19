5b5d6e01ec8ebbfd3aa1bf801cf60e6e2f20cc81
Bobby Earl <bobby.earl@blackbaud.com>
2018-10-19 11:03:54 -0400

    Multiple runs

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const { getChromeDriverVersion } = require('chromedriver-version-matcher');
 
-getChromeDriverVersion().then(result => console.log(result)).catch(err => console.error(err));
+function run(count) {
+  console.log('Run', count, 'start');
+  getChromeDriverVersion().then(result => {
+    console.log('Run', count, 'success');
+    console.log(result);
+  }).catch(err => {
+    console.log('Run', count, 'error');
+    console.error(err);
+  });
+}
+
+for (let i = 0; i < 3; i++) {
+  run(i);
+}
